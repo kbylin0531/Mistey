72c14b128da9adc95b0d3956a2241e2e74122c3a
lichtung <784855684@qq.com>
2015-10-11 10:38:20 +0800

    home

---
+++ Runtime/cache/Home/Index/compile/6c78202048b30f38c42b4f6661ba296d23a40c21_0.file.index.html.php
@@ -0,0 +1,38 @@
+<?php /* Smarty version 3.1.24, created on 2015-10-11 10:32:25
+         compiled from "F:/Web/Webroot/Mist/Application/Home/View/Index/index.html" */ ?>
+<?php
+/*%%SmartyHeaderCode:166555619ca393fb646_17939273%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6c78202048b30f38c42b4f6661ba296d23a40c21' => 
+    array (
+      0 => 'F:/Web/Webroot/Mist/Application/Home/View/Index/index.html',
+      1 => 1441668118,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '166555619ca393fb646_17939273',
+  'has_nocache_code' => false,
+  'version' => '3.1.24',
+  'unifunc' => 'content_5619ca396b76e7_30093966',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_5619ca396b76e7_30093966')) {
+function content_5619ca396b76e7_30093966 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '166555619ca393fb646_17939273';
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<div id="myTime"> <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="200" height="80" id="honehoneclock" align="middle"> <param name="allowScriptAccess" value="always"> <param name="movie" value="http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_wh.swf"> <param name="quality" value="high"> <param name="bgcolor" value="#ffffff"> <param name="wmode" value="transparent"> <embed wmode="transparent" src="http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_wh.swf" quality="high" bgcolor="#ffffff" width="200" height="80" name="honehoneclock" align="middle" allowscriptaccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"> </object> </div>
+</body>
+</html><?php }
+}
+?>
